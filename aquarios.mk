77eebb76a3eb8048ae37c3f228b3efda7b17e3d2
rc420head <dennislebhardt79@gmail.com>
2018-05-17 10:04:06 -0500

    AquariOS

--- aquarios.mk
+++ aquarios.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/h870/full_h870.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aquarios/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := lineage_h870
+PRODUCT_NAME := aquarios_h870
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g6" \
